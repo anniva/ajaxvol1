71fea9e9c0fbba932f3688ef76fbfc27fde5792f
Anni Vaananen <anni.vaananen@gmail.com>
2015-05-31 11:59:14 +0100

    the basics

---
+++ script.js
@@ -0,0 +1,32 @@
+//A syncronous XMLHttopRequest
+
+/* var request= new XMLHttpRequest(); 
+
+request.open('GET','text.txt', false); 
+
+request.send(null);
+
+//console.log(request.responseText);
+
+document.getElementById('response').innerHTML = request.responseText;
+*/
+
+//A Asyncronous XMLHttopRequest
+
+var request= new XMLHttpRequest(); 
+
+request.open('GET','text.txt', true); 
+
+request.send();
+
+//console.log(request.responseText);
+
+request.onreadystatechange = function() { //
+    if(request.readyState == 4) {
+        console.log(request);
+        document.getElementById('response').innerHTML = request.responseText;
+        // this last line grabs the element on the page with the id #response and changes its text to the text from the file that was opened.
+    };
+};
+
+
